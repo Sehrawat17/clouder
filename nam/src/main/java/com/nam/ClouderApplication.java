3f92d7479660aa247969dca9d2ebf9307fb1b641
manish <manish.sehrawat@scoreme.in>
2024-07-11 21:59:30 +0530

    first commit!!

---
+++ nam/src/main/java/com/nam/ClouderApplication.java
@@ -0,0 +1,13 @@
+package com.nam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouderApplication.class, args);
+	}
+
+}
